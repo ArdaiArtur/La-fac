5d213956484e00568ff058055799a9d28ad1e202
ArdaiArtur <72787544+ArdaiArtur@users.noreply.github.com>
2022-06-15 08:14:01 -1200

    Add files via upload

---
+++ cripto2/cripto2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace cripto2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cesar c = new Cesar();
+            c.PlainText = "Helloz";
+            c.Encript();
+
+            Console.WriteLine($"{c.ChiperText}");
+            MAS masc = new MAS();
+        }
+    }
+}
