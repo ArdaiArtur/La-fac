8f807ffa2d24e121f6f1001dbb2d388f7973fd45
ArdaiArtur <72787544+ArdaiArtur@users.noreply.github.com>
2022-06-15 08:13:30 -1200

    Add files via upload

---
+++ cripto2/cripto2/Cesar.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cripto2
+{
+    class Cesar:CSB
+    {
+        public Cesar()
+        {   
+            
+            key = alphabet.Substring(3) + alphabet.Substring(0, 3);
+            
+        }
+    }
+}
