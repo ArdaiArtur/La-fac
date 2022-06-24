ba98f7b4118913f6bc60a3fe0b351436d6a79161
ArdaiArtur <72787544+ArdaiArtur@users.noreply.github.com>
2022-06-24 08:29:39 -1200

    Add files via upload

---
+++ weak2/weak2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace weak2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queque1 A = new Queque1();
+            A.Push(1);
+            A.Push(2);
+            A.Push(3);
+            A.Push(4);
+            A.Push(5);
+            A.Push(6);
+            A.Push(7);
+            A.Push(8);
+            A.Pop();
+            A.Push(1);
+            A.view();
+            Console.ReadKey();
+        }
+    }
+}
