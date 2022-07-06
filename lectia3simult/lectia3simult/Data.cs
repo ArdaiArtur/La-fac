4a2abd5fce503f0929141ef28f35704cce40b615
ArdaiArtur <72787544+ArdaiArtur@users.noreply.github.com>
2022-07-06 21:26:58 +0300

    Add files via upload

---
+++ lectia3simult/lectia3simult/Data.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lectia3simult
+{
+    class Data
+    {
+        public int l, v, c;
+        public Data(int l,int v,int c)
+        {
+            this.l = l;
+            this.v = v; 
+            this.c = c;
+        }
+        public string view()
+        {
+            return "("+l+" "+c+" "+v+")";
+        }
+    }
+}
---
+++ lectia3simult/lectia3simult/Queue.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lectia3simult
+{
+    class Queue
+    {
+        Data[] values;
+
+        public Queue()
+        {
+            values = new Data[0];
+        }
+        public bool isEmpty()
+        {
+            if (values.Length == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        public void Push(Data ValueToAdd)
+        {
+            Data[] temp = new Data[values.Length + 1];
+            for (int i = 0; i < values.Length; i++)
+                temp[i + 1] = values[i];
+            temp[0] = ValueToAdd;
+            values = temp;
+        }
+
+        public Data Pop()
+        {
+            Data toReturn = values[values.Length - 1];
+            Data[] temp = new Data[values.Length - 1];
+            for (int i = 0; i < values.Length - 1; i++)
+                temp[i] = values[i];
+            values = temp;
+            return toReturn;
+        }
+
+        public void view()
+        {
+            for (int i = 0; i < values.Length; i++)
+                Console.Write(values[i].view());
+        }
+    }
+}
+
