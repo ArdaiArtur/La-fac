c855beb29622e2a78271a49b5730ef9e854ed603
ArdaiArtur <72787544+ArdaiArtur@users.noreply.github.com>
2022-07-07 21:57:17 +0300

    Add files via upload

---
+++ l4.3/l4.3/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace l4._3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double con, f;
+            f = Convert.ToDouble(textBox1.Text);
+            con = (((f - 32)) * 5 / 9);
+            textBox2.Text = con.ToString();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox2.ResetText();
+            textBox2.Text = "0";
+            textBox1.ResetText();
+            
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
