0f7de76bba133527d7501af4fe3cbc0033022828
ArdaiArtur <72787544+ArdaiArtur@users.noreply.github.com>
2022-08-02 21:47:06 +0300

    Add files via upload

---
+++ TTcsharp/TTcsharp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TTcsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            person pre = new person();
+           // Console.WriteLine(pre.id);
+            float f = 10.12f;
+            long l = 200L;
+            Console.WriteLine(f + l);
+            int i = 2;
+            Console.WriteLine(++i);
+            Console.WriteLine(i++);
+            Console.WriteLine(i);
+
+        }
+    }
+    public class person
+    {
+        public int id;
+    }
+}
+
